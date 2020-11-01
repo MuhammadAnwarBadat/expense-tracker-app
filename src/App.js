0c5e3da6975ec8accadc3b4e87cfc1cd8d9d597a
anwar_badat <anwarbadat1997.ab@gmail.com>
2020-11-01 14:11:12 +0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Balance from './components/Balance';
+import AddTransaction from './components/AddTransaction';
+import IncomeList from './components/IncomeList';
+import ExpenseList from './components/ExpenseList';
+import { GlobalContextProvider } from './context/GlobalState';
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <GlobalContextProvider>
+    <div className="container">
+      <div className="app-wrapper">
+        <Header />
+        <Footer />
+        <Balance />
+        <AddTransaction />
+        <IncomeList />
+        <ExpenseList />
+      </div>
     </div>
+    </GlobalContextProvider>
   );
-}
+};
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <h3>©Copyright 2020 by Anwar Badat. All Rights Reserved.</h3>
+        </div>
+    );
+};
+
+export default Footer;
